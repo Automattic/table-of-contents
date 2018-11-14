5b3da29a3831ac7b10526394c18320b7a4004b4b
Jake Spurlock <whyisjake@gmail.com>
2018-11-14 14:34:37 -0800

    Add the version

--- table-of-contents.php
+++ table-of-contents.php
@@ -3,7 +3,8 @@
  * Plugin Name: Table of Contents
  * Description: Adds a table of contents to your pages based on h3 and h4 tags. Useful for documention-centric sites.
  * Author: Automattic
- *
+ * Author URI: http://automattic.com/
+ * Version: 0.5.1
  * License: GPL v2
  *
  * @package table-of-contents
