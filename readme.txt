616ad976f1c665fe0c5fe6f19af74d00768a775d
Jake Spurlock <whyisjake@gmail.com>
2018-11-14 14:25:01 -0800

    Add some updates, and the tested up to tag for 5.0 and Gutenberg.

---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Table of Contents ===
+Contributors: Automattic
+Tags: TOC, documentation
+Requires at least: 4.0
+Tested up to: 5.0
+Requires PHP: 5.6
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a table of contents to your pages based on h3 and h4 tags. Useful for documention-centric sites.
+
+== Description ==
+
+Adds a table of contents to your pages based on h3 and h4 tags. Useful for documention-centric sites.
+
+== Installation ==
+1. Upload "test-plugin.php" to the "/wp-content/plugins/" directory.
+1. Activate the plugin through the "Plugins" menu in WordPress.
+
+== Changelog ==
+= 0.5.1 =
+* Testing for 5.0
+* Testing for Gutenberg
+
+= 0.5.0 =
+* Initial release.
--- table-of-contents.php
+++ table-of-contents.php
@@ -1,87 +1,135 @@
-<?php
+<?php //phpcs:ignore
 /**
  * Plugin Name: Table of Contents
  * Description: Adds a table of contents to your pages based on h3 and h4 tags. Useful for documention-centric sites.
  * Author: Automattic
  *
  * License: GPL v2
+ *
+ * @package table-of-contents
  */
 
-if ( ! class_exists( 'Table_Of_Contents' ) ):
+if ( ! class_exists( 'Table_Of_Contents' ) ) :
 
-class Table_Of_Contents {
-	function init() {
-		add_action( 'template_redirect', array( __CLASS__, 'load_filters' ) );
-	}
+	/**
+	 * Table of Contents
+	 */
+	class Table_Of_Contents {
 
-	function load_filters() {
-		if ( is_page() ) {
-			add_filter( 'the_content', array( __CLASS__, 'add_overview_h3' ) );
-			add_filter( 'the_content', array( __CLASS__, 'add_toc' ) );
+		/**
+		 * Kick off the class.
+		 *
+		 * @return void
+		 */
+		public static function init() {
+			add_action( 'template_redirect', array( __CLASS__, 'load_filters' ) );
 		}
-	}
 
-	function add_toc( $content ) {
-		$toc = '';
-		$h3s = self::get_tags( 'h3', $content );
-		$h4s = self::get_tags( 'h4', $content );
-		$items = $h3s + $h4s;
-
-		$content = self::add_ids_and_jumpto_links( 'h3', $content );
-		$content = self::add_ids_and_jumpto_links( 'h4', $content );
-
-		if ( $items ) {
-			$toc .= '<div class="vip-lobby-toc">';
-			$toc .= '<h3>Contents</h3><ul class="items">';
-			foreach ($items as $item) {
-				$toc .= '<li><a href="#' . sanitize_title_with_dashes($item[2])  . '">' . $item[2]  . '</a></li>';
+		/**
+		 * Load the filters.
+		 *
+		 * @return void
+		 */
+		public static function load_filters() {
+			if ( is_page() ) {
+				add_filter( 'the_content', array( __CLASS__, 'add_overview_h3' ) );
+				add_filter( 'the_content', array( __CLASS__, 'add_toc' ) );
 			}
-			$toc .= '</ul>';
-			$toc .= '</div>';
 		}
 
-		return $toc . $content;
-	}
+		/**
+		 * Add the TOC to the top of the page.
+		 *
+		 * @param string $content Page content.
+		 *
+		 * @return string
+		 */
+		public static function add_toc( $content = '' ) {
+			$toc   = '';
+			$h3s   = self::get_tags( 'h3', $content );
+			$h4s   = self::get_tags( 'h4', $content );
+			$items = $h3s + $h4s;
 
-	function add_overview_h3( $content ) {
-		$h3s = self::get_tags( 'h3', $content );
-		if ( ! empty( $h3s ) )
-			$content = "<h3>Overview</h3>\n" . $content;
-		return $content;
-	}
+			$content = self::add_ids_and_jumpto_links( 'h3', $content );
+			$content = self::add_ids_and_jumpto_links( 'h4', $content );
 
-	private function add_ids_and_jumpto_links( $tag, $content ) {
-		$items = self::get_tags( $tag, $content );
-		$first = true;
-
-		foreach ($items as $item) {
-			$replacement = '';
-			$matches[] = $item[0];
-			$id = sanitize_title_with_dashes($item[2]);
-
-			if ( ! $first ) {
-				$replacement .= '<p class="toc-jump"><a href="#content">&uarr; Top &uarr;</a></p>';
-			} else {
-				$first = false;
+			if ( $items ) {
+				$toc .= '<div class="vip-lobby-toc">';
+				$toc .= '<h3>Contents</h3><ul class="items">';
+				foreach ( $items as $item ) {
+					$toc .= '<li><a href="#' . sanitize_title_with_dashes( $item[2] ) . '">' . $item[2] . '</a></li>';
+				}
+				$toc .= '</ul>';
+				$toc .= '</div>';
 			}
 
-			$replacement .= sprintf( '<%1$s id="%2$s">%3$s <a href="#%2$s" class="anchor">#</a></%1$s>', $tag, $id, $item[2] );
-			$replacements[] = $replacement;
+			return $toc . $content;
 		}
 
-		$content = str_replace( $matches, $replacements, $content );
+		/**
+		 * Add an overview to the top of the page.
+		 *
+		 * @param string $content Page content.
+		 *
+		 * @return string
+		 */
+		public static function add_overview_h3( $content ) {
+			$h3s = self::get_tags( 'h3', $content );
+			if ( ! empty( $h3s ) ) {
+				$content = "<h3>Overview</h3>\n" . $content;
+			}
+			return $content;
+		}
 
-		return $content;
+		/**
+		 * Add IDs and anchor links to the headings.
+		 *
+		 * @param string $tag     Tag to add anchor to.
+		 * @param string $content Post content.
+		 *
+		 * @return string
+		 */
+		private static function add_ids_and_jumpto_links( $tag, $content ) {
+			$items = self::get_tags( $tag, $content );
+			$first = true;
+
+			foreach ( $items as $item ) {
+				$replacement = '';
+				$matches[]   = $item[0];
+				$id          = sanitize_title_with_dashes( $item[2] );
+
+				if ( ! $first ) {
+					$replacement .= '<p class="toc-jump"><a href="#content">&uarr; Top &uarr;</a></p>';
+				} else {
+					$first = false;
+				}
+
+				$replacement   .= sprintf( '<%1$s id="%2$s">%3$s <a href="#%2$s" class="anchor">#</a></%1$s>', $tag, $id, $item[2] );
+				$replacements[] = $replacement;
+			}
+
+			$content = str_replace( $matches, $replacements, $content );
+
+			return $content;
+		}
+
+		/**
+		 * Get the tags from post_content
+		 *
+		 * @param string $tag     Tag to search for.
+		 * @param string $content Post content.
+		 *
+		 * @return array
+		 */
+		private static function get_tags( $tag, $content = '' ) {
+			if ( empty( $content ) ) {
+				$content = get_the_content();
+			}
+			preg_match_all( "/(<{$tag}>)(.*)(<\/{$tag}>)/", $content, $matches, PREG_SET_ORDER );
+			return $matches;
+		}
 	}
 
-	private function get_tags( $tag, $content = '' ) {
-		if ( empty( $content ) )
-			$content = get_the_content();
-		preg_match_all( "/(<{$tag}>)(.*)(<\/{$tag}>)/", $content, $matches, PREG_SET_ORDER );
-		return $matches;
-	}
-}
-
-Table_Of_Contents::init();
+	Table_Of_Contents::init();
 
 endif;
